518b187629709d240a47ed786223d0bafed80aa9
umutkavakli <umutkavakli.ukk@gmail.com>
2023-02-05 03:34:56 +0300

    Removing unnecessary comments

--- interface_design.py
+++ interface_design.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file '/home/umut/Documents/integrated-machine-learning/interface.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.1
-#
-# WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
