2d9dfe5c8f603ba3136f3b567af912fc862e8e26
umutkavakli <umutkavakli.ukk@gmail.com>
2023-02-08 02:13:39 +0300

    new features added in gui

--- gui.py
+++ gui.py
@@ -1,21 +1,21 @@
 from PyQt5.QtWidgets import *
 from interface_design import Ui_MainWindow
-from model import Model
+from model_loader import ModelLoader
 import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
 
 class GUI(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.click_button)
-        self.model = Model()
-        self.model.preprocessing()
-        self.model.train_model()
+        self.model = ModelLoader()
 
-    def predict_output(self, sac: float, akma: float, cekme: float, kalip: float):
-        self.ui.output_text.setText(str(self.model.model_predict(np.array([sac, akma, cekme, kalip]).reshape(1, -1))))
+    def predict_output(self, sheet_thickness: float, yield_strength: float, tensile_strength: float, mold_weight: float):
+        self.ui.output_text.setText(str(self.model.model.predict(np.array([sheet_thickness, yield_strength, tensile_strength, mold_weight]).reshape(1, -1))))
  
 
     def click_button(self):
@@ -24,15 +24,10 @@ class GUI(QMainWindow):
         tensile_strength = float(self.ui.tensile_text.toPlainText())
         mold_weight = float(self.ui.mold_text.toPlainText())
 
+        self.model.set_model(self.ui.comboBox.currentText())
+
         self.predict_output(sheet_thickness, yield_strength, tensile_strength, mold_weight)
 
-    # def web_server(self, sheet_thickness, yield_strength, tensile_value, mold_value):
-    #     self.ui.sac_text.setText(sheet_thickness)
-    #     self.ui.akma_text.setText(yield_strength)
-    #     self.ui.cekme_text.setText(tensile_value)
-    #     self.ui.kalip_text.setText(mold_value)
-
-    #     self.predict_output(sheet_thickness, yield_strength, tensile_value, mold_value)
 
 if __name__ == '__main__':
     app = QApplication([])
